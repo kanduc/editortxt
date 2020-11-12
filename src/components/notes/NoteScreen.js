ffc8653d865905b5df2a72469d55305215b8a1db
Kanduc <katluvh@gmail.com>
2020-11-11 21:17:06 -0500

    fix textarea

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,17 +2,8 @@ import React from 'react'
 import { NotesAppBar } from './NotesAppBar'
 import { NotesContador } from './NotesContador'
 import { NotesFormatBar } from './NotesFormatBar'
-import { useSelector } from 'react-redux';
-import { useForm } from '../../hooks/useForm';
 
 export const NoteScreen = () => {
-    const {text_area} = useSelector( state => state.ui );
-
-    const [values, handleInputChange] = useForm({
-        text_area:'',
-        text_title:''
-    });
-
     return (
         <div className="notes__main-content">
         
@@ -20,27 +11,19 @@ export const NoteScreen = () => {
         <NotesFormatBar />
         <div className="notes__content">
 
-        <form>
-
         <input 
             type="text"
             placeholder="Algún título"
-            value={text_title}
             className="notes__title-input"
-            onChange={handleInputChange}
 
         />
 
         <textarea
        placeholder="Escriba Aquí"
         className="notes__textarea"
-        value={text_area}
-        onChange={handleInputChange}
         >
 {/*  placeholder="What happened today" */}
         </textarea>
-
-        </form>
 
       {/*   <div className="notes__image">
             <img 
