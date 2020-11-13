affc2c4d3f3bc5885eee0ab7d28b619c52f5a434
Junior Medrano <junior.med19@gmail.com>
2020-11-13 01:26:32 -0500

    Create new Document1

---
+++ src/helpers/loadNote.js
@@ -0,0 +1,25 @@
+import { db } from "../firebase/firebase-config"
+
+
+//se necesitará el uid para cargar el documento nuevo
+export const loadNote = async (uid) => {
+
+
+  const docSnap= await db.collection(`${uid}/journal/notes`).get();//retorna una promesa;
+  const documents=[];
+
+ /*  console.log(docSnap); */
+  docSnap.forEach(docHijo=>{
+      /* console.log(docHijo.data()); */ //body, title, id
+
+      //incluímos el id
+      documents.push({
+          id:docHijo.id,
+          ...docHijo.data(),
+      })
+
+
+  });
+ /*  console.log(documents); */
+  return documents;
+}
