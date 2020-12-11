158161da8c363b1557632a3bc9c60c33c9b5b916
Nelson Jaimes <nelsonjaimesgonzales@gmail.com>
2020-12-10 21:29:45 -0500

    update 20201210

--- src/menu/TextEditor.js
+++ src/menu/TextEditor.js
@@ -1,46 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState, convertToRaw,ContentState, convertFromHTML} from "draft-js";
+import { useDispatch, useSelector } from 'react-redux';
+import { startSaveNote } from '../actions/notes';
+
 
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
 
-export default class TextEditor extends Component {
+const TextEditor = () =>{
+  const dispatch = useDispatch();
+  const {active} = useSelector( state => state.notes);
+  const {title, body} = active
+  
+  let initialState;
+        if (body.content) {
+           // const processedHTML = DraftPasteProcessor.processHTML(this.props.content);
+            const contentState = ContentState.createFromBlockArray(body.content);
+            //move focus to the end. 
+            initialState = EditorState.createWithContent(contentState);
+            initialState = EditorState.moveFocusToEnd(initialState);
+        }
+        else {
+            initialState = EditorState.createEmpty();
+        }
+  const [editorState, setEditorState] = useState(() => initialState,);
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      editorState: EditorState.createWithContent(
-        ContentState.createFromBlockArray(
-          convertFromHTML('<p>Mensaje inicial en el editor Diplo.</p>')
-        )
-      ),
-    }
-  }
-
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
+  const onEditorStateChange = (state) => {
+    setEditorState(state)
+    var data = draftToHtml(convertToRaw(state.getCurrentContent()));
+    active.body = data;
+    dispatch(startSaveNote(active.id));
+    console.log(data);
   };
 
-  render() {
-    const { editorState } = this.state;
-    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-    return (
-      <div>
-        <Editor
-          editorState={editorState}
-          toolbarClassName="toolbarClassName"
-          wrapperClassName="wrapperClassName"
-          editorClassName="editorClassName"
-          onEditorStateChange={this.onEditorStateChange}
-        />
-        <textarea
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        ></textarea>
-      </div>
-    );
-  }
+   return (
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
 }
+export default TextEditor;
