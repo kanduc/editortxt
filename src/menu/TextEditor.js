1ebc0346e25b74b4615bb9ab9921b0991db34557
Nelson Jaimes <nelsonjaimesgonzales@gmail.com>
2020-12-10 22:50:12 -0500

    update 20201210-2250

--- src/menu/TextEditor.js
+++ src/menu/TextEditor.js
@@ -13,16 +13,19 @@ const TextEditor = () =>{
   const {active} = useSelector( state => state.notes);
   const {title, body} = active
   
-  let initialState;
-        if (body.content) {
-           // const processedHTML = DraftPasteProcessor.processHTML(this.props.content);
-            const contentState = ContentState.createFromBlockArray(body.content);
-            //move focus to the end. 
-            initialState = EditorState.createWithContent(contentState);
-            initialState = EditorState.moveFocusToEnd(initialState);
+  console.log(body);
+  var initialState;
+        if (body=="''") {
+          console.log("Esta vacio !!! ")
+          initialState = EditorState.createEmpty();
         }
         else {
-            initialState = EditorState.createEmpty();
+          console.log("No está vacio...")
+          // const processedHTML = DraftPasteProcessor.processHTML(this.props.content);
+           const contentState = ContentState.createFromBlockArray(convertFromHTML("<p>e32e23ee32<span style='color: rgb(209,72,65);'>eeees</span>z<span style='color: rgb(44,130,201);'>HOLA A TODOS LODE MSS <strong>DWQDWQ</strong></span></p>"));
+           //move focus to the end. 
+           initialState = EditorState.createWithContent(contentState);
+           initialState = EditorState.moveFocusToEnd(initialState);
         }
   const [editorState, setEditorState] = useState(() => initialState,);
 
