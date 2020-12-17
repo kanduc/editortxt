5e30fad105c72a8686929da97f36fb7928b6b227
Katty <katluvh@gmail.com>
2020-12-17 02:17:34 -0500

    Update TextEditorR.js

--- src/menu/TextEditorR.js
+++ src/menu/TextEditorR.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { startSaveNote } from '../actions/notes';
+import { textForm } from '../../hooks/textForm';
 import ReactQuill from "react-quill";
 import EditorToolBar, { modules, formats } from "./EditorToolBar";
 import "react-quill/dist/quill.snow.css";
@@ -12,25 +13,28 @@ const TextEditorR = () =>{
 	const dispatch = useDispatch();
 	const {active} = useSelector( state => state.notes);
 	const {title, body} = active
+	const {active:docNew} = useSelector( state => state.notes );
+   	const  [stateEditor, handleTextChange]= textForm(docNew);
+   	const {body,id}=stateEditor;
 	console.log("body text "+body);
 	//const [stateEditor, setStateEditor] = useState(() =>  ,);
-	const [stateEditor, setStateEditor] = React.useState({ text: body });
+	//const [stateEditor, setStateEditor] = React.useState({ text: body });
 	console.log("inicio text")
-	console.log(stateEditor.text)
-	const handleChange = text => {
+	//console.log(stateEditor.text)
+	/*const handleChange = text => {
 		console.log("handleChange");
 		console.log(text)
 		active.body = text;
 	    setStateEditor({ text });
 	    dispatch(startSaveNote(active));
-	};
+	};*/
 	return (
 	    <div className="text-editor">
 	      <EditorToolBar />
 	      <ReactQuill
 	        theme="snow"
-	        value={stateEditor.text}
-	        onChange={handleChange}
+	        value={body}
+	        onChange={handleTextChange}
 	        placeholder={"Escriba algo..."}
 	        modules={modules}
 	        formats={formats}
