f49e35d6a71c0e46b9a9c730e4b44461dc216599
Katty <katluvh@gmail.com>
2020-12-17 02:41:25 -0500

    Update TextEditorR.js

--- src/menu/TextEditorR.js
+++ src/menu/TextEditorR.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useRef , useEffect} from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { startSaveNote } from '../actions/notes';
 import { textForm } from '../hooks/textForm';
@@ -14,26 +14,37 @@ const TextEditorR = () =>{
 	/*const {active} = useSelector( state => state.notes);
 	const {title, body} = active*/
 	const {active:docNew} = useSelector( state => state.notes );
-   	const  [stateEditor, handleTextChange]= textForm(docNew);
-   	const {body,id}=stateEditor;
+   	//const  [stateEditor, handleTextChange]= textForm(docNew);
+   	const {body,id}=docNew;
 	console.log("body text "+body);
 	//const [stateEditor, setStateEditor] = useState(() =>  ,);
-	//const [stateEditor, setStateEditor] = React.useState({ text: body });
+	const [stateEditor, setStateEditor] = useState({ value: body });
 	console.log("inicio text")
-	//console.log(stateEditor.text)
-	/*const handleChange = text => {
+	console.log(stateEditor.text)
+	const activeId = useRef( docNew.id );
+	
+	/*
+	useEffect(() => {
+	   //ejecutar la acción si o solo sí el id es diferente
+		if(docNew.id!==activeId.current){
+		    setStateEditor({ value : docNew.body });
+		    activeId.current=docNew.id
+		}
+	}, [docNew])*/
+	
+	const handleChange = text => {
 		console.log("handleChange");
 		console.log(text)
 		active.body = text;
-	    setStateEditor({ text });
+	    setStateEditor({ value : text });
 	    dispatch(startSaveNote(active));
-	};*/
+	};
 	return (
 	    <div className="text-editor">
 	      <EditorToolBar />
 	      <ReactQuill
 	        theme="snow"
-	        value={body}
+	        value={stateEditor.value}
 	        onChange={handleTextChange}
 	        placeholder={"Escriba algo..."}
 	        modules={modules}
